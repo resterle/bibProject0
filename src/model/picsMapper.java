cdb59914f3cfb0cc5b24e3d0ea2f7d13294d8a61
bbth8bfr <bbth8bfr@pc0g-18.bg.bib.de>
2011-02-08 12:43:30 +0100

    credits button

--- src/model/picsMapper.java
+++ src/model/picsMapper.java
@@ -7,6 +7,6 @@ public class picsMapper {
 	public static final String NEW_GAME_BUTTON = path+"start.png";
 	public static final String HIGHSCORE_BUTTON = path+"high.png";
 	public static final String OPTIONS_BUTTON = path+"option.png";
-	public static final String CREDITS_BUTTON = path+"credits.png";
+	public static final String CREDITS_BUTTON = path+"button_credits.png";
 	public static final String QUIT_BUTTON = path+"quit.png";
 }

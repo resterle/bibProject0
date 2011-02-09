6bacb16b1c53767a1f21d2b49eaeff35fc688b2c
bbth8bti <bbth8bti@pc0g-16.bg.bib.de>
2011-02-09 14:14:46 +0100

    Comments added

--- src/activities/PlayernameActivity.java
+++ src/activities/PlayernameActivity.java
@@ -14,11 +14,15 @@ import view.Activity;
 
 public class PlayernameActivity extends Activity {
 
+	//Keys for returning Values to Controller
 	public final static String RETURN_NAME = "DefaultName";
 	public final static String RETURN_GALLARY = "gallary";
 
+	//Keys for receiving Values from Controller
 	public final static String PARAMS_NAME = "DefaultName";
 	
+	
+	//Text Field with the length of 15 chars
 	JTextField playerName = new JTextField(15);
 	
 	
@@ -30,14 +34,16 @@ public class PlayernameActivity extends Activity {
 		
 		
 		
-		
+		//Buttons declaration
 		JButton gallaryB = new JButton("Gallary");
 		JButton ownPicB = new JButton("Use own Picture");
 		
+		//Label declaration
 		JLabel nameL = new JLabel("Enter your name: ");
 		
 		
-		
+		//If player name is null it's setting it to a Default
+		//Else it puts the Name of the last player in
 		if (null == params.getValue(PARAMS_NAME)){			
 			playerName.setText(PARAMS_NAME);
 		}
@@ -46,6 +52,7 @@ public class PlayernameActivity extends Activity {
 		}
 		
 		
+		//Layout declaration
 		JPanel enterName = new JPanel();
 		JPanel Buttons = new JPanel();
 		
@@ -60,6 +67,8 @@ public class PlayernameActivity extends Activity {
 		add(enterName);
 		add(Buttons);
 		
+		
+		//Action what the Buttons shall return to Controller as soon as they are pushed
 		gallaryB.addActionListener(new ActionListener() {
 			
 			
@@ -88,6 +97,8 @@ public class PlayernameActivity extends Activity {
 		
 	}
 	
+	
+	//Method to return the players name to the Controller
 	private void returnName(){
 		
 		returnParams.addParameter(RETURN_NAME, playerName.getText());

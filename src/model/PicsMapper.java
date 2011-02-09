a11508014a46207f65f281f5793588674946446c
bbth8bfr <bbth8bfr@pc0g-18.bg.bib.de>
2011-02-09 10:23:26 +0100

    picsmapper die 2

---
+++ src/model/PicsMapper.java
@@ -0,0 +1,16 @@
+package model;
+
+public class PicsMapper {
+	
+	private static String path = "pics/";
+	
+	public static final String BACKGROUND = path+"puzzle.png";
+	
+
+	public static final String NEW_GAME_BUTTON = path+"push_button/start.png";
+	public static final String HIGHSCORE_BUTTON = path+"push_button/high.png";
+	public static final String OPTIONS_BUTTON = path+"push_button/option.png";
+	public static final String CREDITS_BUTTON = path+"push_button/button_credits.png";
+	public static final String QUIT_BUTTON = path+"push_button/quit.png";
+	public static final String QUITe_BUTTON = path+"push_button/quit.png";
+}
